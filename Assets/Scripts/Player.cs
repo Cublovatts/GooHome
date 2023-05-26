3597674406b008b03e607419758949d0544aee72
Ami <ami1996_tm@live.co.uk>
2023-05-26 11:12:52 +0100

    Added check for player jumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    public bool canJumpMidair = false;
+    [SerializeField]
+    bool isMidair = true; // prefer hasJumped or canJump?
 
     public void Jump(Vector2 dir)
     {
-        rb.constraints = RigidbodyConstraints2D.None;
-        rb.AddForce(dir, ForceMode2D.Impulse);
+        if (canJumpMidair || !isMidair || rb.velocity.magnitude == 0)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.AddForce(dir, ForceMode2D.Impulse);
+            // hasJumped = true;
+        }
     }
 
     void Start()
@@ -27,5 +34,16 @@ public class Player : MonoBehaviour
         // TODO: Check for different collider types
         // E.g. if (collider is sticky)
         rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        isMidair = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isMidair = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isMidair = true;
     }
 }
