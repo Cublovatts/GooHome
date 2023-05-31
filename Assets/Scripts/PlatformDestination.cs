95184436c6929cc914359d7ba107cd2f1e784636
Cublovatts <george.a.lovatt@gmail.com>
2023-05-31 11:12:53 +0100

    Added a moving platform script

---
+++ Assets/Scripts/PlatformDestination.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class PlatformDestination
+{
+    public GameObject DestinationObject;
+    public float MovementSpeed;
+    public float DelayOnArrival;
+}
