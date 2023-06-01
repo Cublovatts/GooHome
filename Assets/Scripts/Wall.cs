2b3820d128c91b5184f590c7c7a96779d2c9ec12
Ami <ami1996_tm@live.co.uk>
2023-06-01 09:56:13 +0100

    Added bouncy type in Wall script

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum WALL_TYPE { normal, sticky, slippy }
+public enum WALL_TYPE { normal, sticky, slippy, bouncy }
 
 public class Wall : MonoBehaviour
 {
