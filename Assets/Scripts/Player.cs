fb5250c276fdbc9f50de0c9983d655df7eab3aa5
Ami <ami1996_tm@live.co.uk>
2023-06-01 09:26:07 +0100

    Wall script with different types

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     {
         if (canJumpMidair || !isMidair || rb.velocity.magnitude == 0)
         {
-            //rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.None;
             rb.AddForce(dir, ForceMode2D.Impulse);
             // hasJumped = true;
         }
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WALL_TYPE { normal, sticky, slippy }
+
+public class Wall : MonoBehaviour
+{
+    public WALL_TYPE type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb;
+        bool hasRb = collision.gameObject.TryGetComponent<Rigidbody2D>(out rb);
+
+        switch (type)
+        {
+            case WALL_TYPE.sticky:
+                if(hasRb)
+                    rb.constraints = RigidbodyConstraints2D.FreezePosition;
+                break;
+
+            case WALL_TYPE.slippy:
+                break;
+
+            case WALL_TYPE.normal:
+            default:
+                break;
+        }
+    }
+}
